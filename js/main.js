b733a32da23a7954f51a2679698223f92bc2b9f1
Brandon Mushore <you@example.com>
2020-07-01 06:58:15 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,71 @@
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//     anchor.addEventListener('click', function (e) {
+//         e.preventDefault();
+
+//         document.querySelector(this.getAttribute('href')).scrollIntoView({
+//             behavior: 'smooth'
+//         });
+//     });
+// });
+
+// $(document).ready(function() {
+//     $(".dropdown-toggle").dropdown();
+// });
+$('.currency-container').slideToggle();
+$(".dollar").click(function(){
+    $(".currency-container").slideToggle();
+  });
+
+
+
+  var owl= $('.owl-carousel');
+
+var carouselOption={
+    items:1,
+    // loop:true,
+    margin:10,
+    autoplay:false,
+    autoplayTimeout:500,
+    nav:true,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1,
+            autoplay:false
+        },
+        768:{
+            items:1,
+            autoplay:false
+        }
+       
+    }
+
+}
+owl.owlCarousel(carouselOption);
+  
+
+
+// var mybutton = document.getElementById("myBtn");
+
+// // When the user scrolls down 20px from the top of the document, show the button
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
+
+// // When the user clicks on the button, scroll to the top of the document
+// function topFunction(e) {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// }
+
+$("#myBtn").click(function() {
+    $('html,body').animate({
+        scrollTop: $("nav").offset().top},
+        'slow');
+});
