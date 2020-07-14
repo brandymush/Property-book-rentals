65be755e6c8c4bbcf46f3c74ad45874e8ca9cf92
Brandon Mushore <you@example.com>
2020-07-15 00:42:10 +0200

    listed

--- js/main.js
+++ js/main.js
@@ -112,3 +112,18 @@ function myFunction(imgs) {
 
 
 }
+
+
+
+function myFunction(imgs){
+  // Get the expanded image
+  var expandImg = document.getElementById("expandedImg");
+  // Get the image text
+
+  // Use the same src in the expanded image as the image being clicked on from the grid
+  expandImg.src = imgs.src;
+  // Use the value of the alt attribute of the clickable image as text inside the expanded image
+
+  // Show the container element (hidden with CSS)
+  // expandImg.parentElement.style.display = "block";
+}
