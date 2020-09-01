564b424655f61dbebbcbbe151d5c927d877b0c93
brandymush <brandon@sproutcoding.org>
2020-09-01 17:28:42 +0200

    properties

--- js/main.js
+++ js/main.js
@@ -62,7 +62,9 @@ $('.carousel').carousel({
     interval: false,
   });
 
-
+window.onload = function() {
+  $(".currency-container").hide();
+};
 
 document.onscroll = function() {
         if (window.innerHeight + window.scrollY > document.body.clientHeight) {
