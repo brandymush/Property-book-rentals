9e9fd3d7a144b2d2847d5e88f6556120e3f9f4cb
brandymush <brandon@sproutcoding.org>
2020-10-07 16:48:20 +0200

    all

--- js/js.js
+++ js/js.js
@@ -1,13 +1,13 @@
 
 $('.carousel').carousel();
 
-var owl= $('.owl-carousel-2');
+var owl= $('.owl-carousel');
 
 var carouselOption={
    items:3,
     loop:true,
     margin:10,
-    autoplay:false,
+    autoplay:true,
     autoplayTimeout:400000,
        nav:true,
     navigation:true,
@@ -15,20 +15,24 @@ var carouselOption={
     autoplayHoverPause:true,
     responsive:{
         0:{
-          items:3,
-          //  items:1,
-          //   nav:true,
-          //   navText: ['<span class="fas fa-chevron-left fa-2x"></span>','<span class="fas fa-chevron-right fa-2x"></span>'], 
+       items:1,
         },
         768:{
-          items:3,   
+          items:2,   
+        },
+        1024:{
+          items:3,
         },
         1440:{
-     
+         nav:true,
+         navigation:true,
+           nav:true,
+    navigation:true,
+    navText: ['<span class="fa fa-angle-left"></span>','<span class="fa fa-angle-right"></span>'],
          
         },
         576:{
-        
+            items:1,
             nav:true,
             
         }
